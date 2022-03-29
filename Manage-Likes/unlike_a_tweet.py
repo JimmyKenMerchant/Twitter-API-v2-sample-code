56b6cd1ef9b7502fda2269bd7dae3e38e853e5ff
Kenta Ishii <k.i.drivingroad21@gmail.com>
2022-03-29 15:49:09 +0900

    Modified Manage-Likes/unlike_a_tweet.py
    1. Delete with JSON File Made from Archive
    
    2. Fit with Latest Rate Limits
    
    3. Search by Regular Expression

--- Manage-Likes/unlike_a_tweet.py
+++ Manage-Likes/unlike_a_tweet.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
 from requests_oauthlib import OAuth1Session
 import os
 import json
+import time
+import datetime
+import re
+import sys
 
 # In your terminal please set your environment variables by running the following lines of code.
 # export 'CONSUMER_KEY'='<your_consumer_key>'
@@ -10,14 +15,20 @@ consumer_secret = os.environ.get("CONSUMER_SECRET")
 
 # Be sure to replace your-user-id with your own user ID or one of an authenticated user
 # You can find a user ID by using the user lookup endpoint
-id = "your-user-id"
+user_id = 'your-user-id'
+sleep_time_second = 20 # Rate Limit for Deleting Likes = 50 Requests per 15 Minutes Window
+delete_tweet_first_index = 0
+max_search_tweet = 1000
+regex_str = 'your-string-to-undo-like-by-regex'
+regex_flags = re.I|re.M
+json_file = open('like.json', 'r')
+json_data = json.load(json_file)
 
-# You can replace Tweet ID given with the Tweet ID you wish to like.
-# You can find a Tweet ID by using the Tweet lookup endpoint
-tweet_id = "1354143047324299264"
+print("Number of Likes: " + str(len(json_data)))
+print("Regular Expression: \'" + regex_str + "\'")
 
 # Get request token
-request_token_url = "https://api.twitter.com/oauth/request_token"
+request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
 oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
 
 try:
@@ -59,19 +70,37 @@ oauth = OAuth1Session(
     resource_owner_secret=access_token_secret,
 )
 
-# Making the request
-response = oauth.delete(
-    "https://api.twitter.com/2/users/{}/likes/{}".format(id, tweet_id)
-)
+count_delete = 0
 
-if response.status_code != 200:
-    raise Exception(
-        "Request returned an error: {} {}".format(
-            response.status_code, response.text)
-    )
+for i in range(delete_tweet_first_index, delete_tweet_first_index + max_search_tweet):
+    if i >= len(json_data):
+        print("No Likes Anymore!")
+        sys.exit()
+    tweet_id = json_data[i]['like']['tweetId']
+    like_text = repr(json_data[i]['like']['fullText']) # Make Raw String like r'string'
+    if re.search(regex_str, like_text, regex_flags) == None:
+        continue
+    print("--------------------------------")
+    print("Index: " + str(i))
+    print("Tweet ID: " + tweet_id)
+    print("Full Text: " + like_text)
+    print("Waiting for " + str(sleep_time_second) + " Seconds...")
+    time.sleep(sleep_time_second)
+    # Making the request
+    response = oauth.delete("https://api.twitter.com/2/users/{}/likes/{}".format(user_id, tweet_id))
 
-print("Response code: {}".format(response.status_code))
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
 
-# Saving the response as JSON
-json_response = response.json()
-print(json.dumps(json_response, indent=4, sort_keys=True))
+    print("Response Code: {}".format(response.status_code))
+
+    # Saving the response as JSON
+    json_response = response.json()
+    response = json_response['data']['liked']
+    #print(json_response)
+    print("Liked: " + str(response))
+    count_delete += 1
+    print("Deleted Likes: " + str(count_delete))
+
